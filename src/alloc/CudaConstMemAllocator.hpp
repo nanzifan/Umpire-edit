8bbcbc0919aa914b04b1865fb7762981e86fa74f
znan <znan@ncsu.edu>
2018-07-06 10:37:50 -0700

    inside namespace

--- src/alloc/CudaConstMemAllocator.hpp
+++ src/alloc/CudaConstMemAllocator.hpp
@@ -18,12 +18,12 @@
 #include <cuda_runtime_api.h>
 // #include "umpire/resource/ConstantMemoryResource.hpp"
 
-// __constant__ char umpire_internal_device_constant_memory[64*1024];
+__constant__ char umpire_internal_device_constant_memory[64*1024];
 
 namespace umpire {
 namespace alloc {
 
-__constant__ char umpire_internal_device_constant_memory[64*1024];
+// __constant__ char umpire_internal_device_constant_memory[64*1024];
 /*!
  * \brief Uses cudaMalloc and cudaFree to allocate and deallocate memory on
  *        NVIDIA GPUs.
